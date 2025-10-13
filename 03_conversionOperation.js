dae4b11f941a3b22838453de1ad642f987a848d4
ajay-7078 <ajayk889087@gmail.com>
2025-10-13 16:47:58 +0530

    conversion

--- 03_conversionOperation.js
+++ 03_conversionOperation.js
@@ -21,7 +21,44 @@ console.log(booleanIsLoggedIn);
 // conversion number to string
 let someNumber = 44
 
-let stringNubmer = string(someNumber)
+// let stringNubmer = string(someNumber)
 
-console.log(stringNumber);
+// console.log(stringNumber);
 // console.log(typeof stringNubmer);
+
+// ***********************Operations*****************************
+ let value= 3
+ let negValue = -value
+//  console.log(negValue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2**2);
+// console.log(2*2);
+// console.log(2%2);
+// console.log(2/2);
+
+
+let str1 = "hello"
+let str2 = "hitesh"
+let str3 = str1+str2
+
+// console.log(str3);
+// console.log("2"+2);
+// console.log(2+2+'3');
+// console.log("2"+2+3);
+ 
+console.log(+true);
+console.log(+"");
+
+
+
+let gameCounter = 100 
+gameCounter++;
+console.log(gameCounter);
+
+
+
+
+
